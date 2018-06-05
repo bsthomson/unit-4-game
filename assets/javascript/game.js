989dcb626f1a3c660696be650a34197341462344
bsthomson <thomson.bs@gmail.com>
2018-06-05 17:47:06 -0500

    chchchanges

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,45 @@ $( document ).ready(function() {
     var loseCounter = 0;
     var centerGoodEmpty = true;
     var centerBadEmpty = true;
+    
+    var travelingSeamstress = {
+        HP: 150,
+        attack: 30,
+        counterAttack: 10,
+    };
+    
+    var evilYetiHP = {
+        HP: 210,
+        attack: 25,
+        counterAttack: 5,
+    };
+
+    var docSkullHP = {
+        HP: 200,
+        attack: 25,
+        counterAttack: 5,
+    }
+
+    var dropBearHP = {
+        HP: 90,
+        attack: 50,
+        counterAttack: 25,
+    };
+
+    var rootHP = {
+        HP: 110,
+        attack: 35,
+        counterAttack: 10,
+    };
+
+    var clamDwarfHP = {
+        HP: 50,
+        attack: 100,
+        counterAttack: 2,
+    };
+
+
+
     // Function to move player's character to middle and start the game
     $(".good").on("click", function() {
         if (centerGoodEmpty==true) {

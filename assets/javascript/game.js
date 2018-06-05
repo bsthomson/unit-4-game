40056f1706e1bc7227434a52288a0a423acf230b
bsthomson <thomson.bs@gmail.com>
2018-06-04 20:53:15 -0500

    chchchanges

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,30 @@
-$(".good").on("click", function() {
-    console.log("good");
-});
+var winCounter = 0;
+var loseCounter = 0;
+var centerGoodEmpty = true;
+var centerBadEmpty = true;
 
-$(document).ready(function() {
-    console.log("Hello, World!");
+$( document ).ready(function() {
+
+    $(".good").on("click", function() {
+        console.log("good");
+    });
+
+    if ("goodduel" === "goodempty"){
+        console.log("goodduel");
+    };
+
+    $(".good").on("click", function() {
+        if (centerGoodEmpty==true) {
+            $(this).clone().appendTo(".goodduel");
+            centerGoodEmpty=false;
+        };
+            alert(centerGoodEmpty);
+            $(".bad").on("click", function() {
+                if (centerBadEmpty==true) {
+                    $(this).clone().appendTo(".badduel");
+                    centerBadEmpty=false;
+                };
+            });
+    });
+        
 });

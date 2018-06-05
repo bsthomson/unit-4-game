ee393b24b35fb8b2a15953250302313516849e7e
bsthomson <thomson.bs@gmail.com>
2018-06-04 20:57:00 -0500

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,21 @@
-var winCounter = 0;
-var loseCounter = 0;
-var centerGoodEmpty = true;
-var centerBadEmpty = true;
-
 $( document ).ready(function() {
 
-    $(".good").on("click", function() {
-        console.log("good");
-    });
-
-    if ("goodduel" === "goodempty"){
-        console.log("goodduel");
-    };
-
+    var winCounter = 0;
+    var loseCounter = 0;
+    var centerGoodEmpty = true;
+    var centerBadEmpty = true;
+    // Function to move player's character to middle and start the game
     $(".good").on("click", function() {
         if (centerGoodEmpty==true) {
             $(this).clone().appendTo(".goodduel");
+            // Ensures player can't pick a new character
             centerGoodEmpty=false;
         };
-            alert(centerGoodEmpty);
+            // Function to move player's enemy to the middle
             $(".bad").on("click", function() {
                 if (centerBadEmpty==true) {
                     $(this).clone().appendTo(".badduel");
+                    // Ensures player can't pick a new enemy
                     centerBadEmpty=false;
                 };
             });
